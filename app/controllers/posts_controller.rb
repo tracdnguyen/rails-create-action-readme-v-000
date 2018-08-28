a5cdb24d1b40be71f38012479e0627e37ddc6fc6
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-28 23:25:03 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,5 +18,6 @@ class PostsController < ApplicationController
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
+    redirect_to post_path(@post)
   end
 end
