711ab3164bd4df57138e90e5fad2cd62f17b6746
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-28 23:19:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,9 @@ class PostsController < ApplicationController
   # add create method here
 
   def create
-
+    post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
   end
 end
